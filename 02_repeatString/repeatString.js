c805a0b2927064dbd94b1e99848c687523fbd57f
mdoute <mdoute@gmail.com>
2024-12-11 08:58:22 -0500

    Completed first 5 of 9 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
+const string = "hey";
 const repeatString = function() {
-
+    let result = "";
+    for (let i = 1; i < 4; i++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+
+    for (let i = 0; i <= end; i++) {
+        sum += i; 
+    }
+
+    return sum
 };
 
 // Do not edit below this line

82e5541f5b1704c95a8b66536142ea2bfcd0ced6
mdoute <mdoute@gmail.com>
2024-12-11 21:11:12 -0500

    Completed 7/7 exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemp) {
+  let celciusTemp = (5/9) * (farenheitTemp - 32);
+  let roundedTemp = Math.round(celciusTemp * 10) / 10
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusTemp) {
+  let farenheitTemp = celciusTemp * (9/5) + 32;
+  let roundedTemp = Math.round(farenheitTemp * 10) / 10
+  return roundedTemp;
 };
 
 // Do not edit below this line
